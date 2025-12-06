4ebc8faeb1829e2d6d9732f5a33fb6a23dbf0dae
Antoine Laborderie <antoine.laborderie@gmail.com>
2025-12-06 13:40:14 +0100

    fix: neotest keymaps overlapping with lazy extra

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- neotest
+vim.keymap.set("n", "<leader>tn", function()
+  require("neotest").run.run(vim.fn.expand("%"))
+end, { desc = "Run test file (neotest)" })
+vim.keymap.set("n", "<leader>tN", function()
+  require("neotest").run.run()
+end, { desc = "Run nearest test (neotest)" })
+vim.keymap.set("n", "<leader>tD", function()
+  require("neotest").run.run({ suite = false, strategy = "dap" })
+end, { desc = "Debug test file (neotest)" })
+vim.keymap.set("n", "<leader>td", function()
+  require("neotest").run.run({ vim.fn.expand("%"), suite = false, strategy = "dap" })
+end, { desc = "Debug nearest test (neotest)" })
